e2fb6edfd3f2c8763b323392ea6940765c67e270
Vivek Eswaran <vivekeswaran@Viveks-MacBook-Pro-2.local>
2018-10-30 14:20:19 -0700

    react custom date picker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Datepicker from "./components/Datepicker";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="w-25 py-5 my-5 mx-auto" style={{ minWidth: "60%" }}>
+        <Datepicker label="Birthday" value="2000-08-15" />
       </div>
     );
   }
